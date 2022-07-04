8160102fa4c87da73474427484115049c3eef4a5
Jacob <jake.diep22@gmail.com>
2022-07-04 03:05:44 -0500

    tweak

--- index.mjs
+++ index.mjs
@@ -1,6 +1,9 @@
 import {} from 'dotenv/config';
-import {Client, Intents} from 'discord.js';
+import fs from 'fs';
+import dedent from 'dedent-js';
+import {Client, Intents, MessageEmbed} from 'discord.js';
 import {HenrikDevValorantAPI} from 'unofficial-valorant-api';
+import { start } from 'repl';
 const VAPI = new HenrikDevValorantAPI();
 
 
@@ -35,25 +38,35 @@ client.on("messageCreate",(message)=>{
         .trim()
         .substring(startOfCom.length)
         .split(/\s+/);
-        if(cmdName=="check")
-        {
-
+        if(message.length===undefined)
+            message.channel.send(">>> Please enter a valid command. Use !!help to see a list of commands.");
+        if(cmdName==="help"){
+            message.channel.send(dedent`>>> ** Command Prefix: !! **
+            List of commands:
+            help: Lists all commands and basic info on bot.
+            check: Displays player name, level, current rank and current rr of a player. Format: !!check name tag
+            `);
+        }
+        if(cmdName== "check"){
             if(args.length != 2)
-                return message.reply('Please provide name and tag of player.');
+                return message.reply('>>> Please provide name and tag of player.');
             else{
                 let version = "v1";
                 let region = "na";
                 async function findMMR(version, region, name, tag){
-                    const test = await VAPI.getMMR({ version, region, name, tag });
-                    if(test.status===200){
-                        message.channel.send(`Current Ranked Information:
+                    const mmrData = await VAPI.getMMR({ version, region, name, tag });
+                    const accountData = await VAPI.getAccount({name,tag});
+                    if(mmrData.status===200){
+                        message.channel.send( {files: [`${accountData.data.card.wide}`]});
+                        message.channel.send(dedent`>>> Current Ranked Information:
                         Player: ${name}#${tag}
-                        Current rank: ${test.data.currenttierpatched}
-                        Current rr:${test.data.ranking_in_tier}`);
-                        
+                        Level: ${accountData.data.account_level}
+                        Current rank: ${mmrData.data.currenttierpatched}
+                        Current rr:${mmrData.data.ranking_in_tier}`
+                        );
                     }
                     else{
-                        message.channel.send("ERROR: Player does not exist or you did not enter the correct name or tag.")
+                        message.channel.send(">>> ERROR: Player does not exist or you did not enter the correct name or tag.")
                     }
                 }
                 findMMR(version, region, args[0], args[1]);
@@ -69,14 +82,14 @@ client.on("messageCreate",(message)=>{
                 addToCount(messagez);
                 if(position==(endLen-1)){
                     var Count= fs.readFileSync('buttercount','utf8');
-                    message.channel.send("Butter has been sent "+ Count +" times.");
+                    message.channel.send(">>> Butter has been sent "+ Count +" times.");
                 }
             }
             else if(messagez[position].includes("🧈"||messagez[position]=="🧈")){
                 addToCount(messagez);
                 if(position==(endLen-1)){
                     var Count= fs.readFileSync('buttercount','utf8');
-                    message.channel.send("Butter has been sent "+ Count +" times.");
+                    message.channel.send(">>> Butter has been sent "+ Count +" times.");
                 }
             }
         }
